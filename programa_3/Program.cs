6c2f419b02a64b8be48a3a7145042da9c196e5fc
Juan Pablo Rubio Mejia <juanpablo.rubio990221@gmail.com>
2023-08-17 16:25:37 -0500

    creo el ejercicio 3 el cual consiste en crear un vehiculo, y tener los emtodos conducir, arrancar y  apagar

---
+++ programa_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Crea una clase para representar un automóvil. 
+La clase debe tener atributos para la marca, 
+modelo, el año, el color y el número de puertas.
+También debe tener métodos para arrancar el automóvil
+conducir el automóvil, apagar el automóvil y cambiar de marcha.*/
+using programa_3;
+
+Console.WriteLine("Programa 3");
+
+automovil carro1 = new automovil("kia", "Sportage", 2016, ColorAutomovil.Rojo, 4);
+
+carro1.Arrancar();
+carro1.Conducir();
+carro1.CambiarMarcha(1);
+carro1.CambiarMarcha(2);
+carro1.CambiarMarcha(1);
+carro1.CambiarMarcha(1);
+carro1.CambiarMarcha(-1);
+
+carro1.Apagar();
+Console.ReadLine();
---
+++ programa_3/automovil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_3
+{
+    // Enum para representar los colores del automóvil
+    public enum ColorAutomovil
+    {
+        Rojo,
+        Azul,
+        Verde,
+        Negro,
+        Blanco,
+        Otro
+    }
+    internal class automovil       
+    {
+        // Atributos de la clase
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public ColorAutomovil Color { get; set; }//asi llamamos el enum
+        public int NumPuertas { get; set; }
+
+        private bool encendido;
+        private int marcha;
+
+        // Constructor de la clase
+        public automovil(string marca, string modelo, int año, ColorAutomovil color, int numPuertas)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Año = año;
+            Color = color;
+            NumPuertas = numPuertas;
+
+            encendido = false;
+            marcha = 0;
+        }
+        // Método para arrancar el automóvil
+        public void Arrancar()
+        {
+            if (!encendido)
+            {
+                Console.WriteLine("El automóvil ha arrancado.");
+                encendido = true;
+            }
+            else
+            {
+                Console.WriteLine("El automóvil ya está encendido.");
+            }
+        }
+
+        // Método para conducir el automóvil
+        public void Conducir()
+        {
+            if (encendido)
+            {
+                Console.WriteLine($"El automóvil {Color.ToString().ToLower()} está en movimiento.");
+            }
+            else
+            {
+                Console.WriteLine("Primero debes arrancar el automóvil.");
+            }
+        }
+
+        // Método para cambiar de marcha
+        public void CambiarMarcha(int nuevaMarcha)
+        {
+            if (encendido)
+            {
+                if (nuevaMarcha<=5 || nuevaMarcha>=-1)
+                {
+                    if ((nuevaMarcha == marcha + 1) || (nuevaMarcha == marcha - 1))
+                    {
+                        marcha = nuevaMarcha;
+                        Console.WriteLine($"Has cambiado a la marcha {nuevaMarcha}.");
+                    }
+                    else if (nuevaMarcha == 0) { Console.WriteLine("El vehiculo esta en neutro"); }
+                    else if (nuevaMarcha == -1) { Console.WriteLine("Esta dando reversa"); }
+                    else { Console.WriteLine("no puedes saltar varias marchas"); }
+                }
+                else { Console.WriteLine("EL vehiculo no tiene mas marchas"); }               
+                
+            }
+            else
+            {
+                Console.WriteLine("Primero debes arrancar el automóvil.");
+            }
+        }
+
+        // Método para apagar el automóvil
+        public void Apagar()
+        {
+            if (encendido)
+            {
+                Console.WriteLine("El automóvil se ha apagado.");
+                encendido = false;
+            }
+            else
+            {
+                Console.WriteLine("El automóvil ya está apagado.");
+            }
+        }
+
+
+    }
+
+}
