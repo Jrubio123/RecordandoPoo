a4baaf41f60d3a3bc6954a91c7a1884bfa2271b4
Juan Pablo Rubio Mejia <juanpablo.rubio990221@gmail.com>
2023-08-17 00:13:23 -0500

    terminamos el el ejercicio 1 y hacemos varias pruebas para probar get y set

--- RecordandoPoo/Program.cs
+++ RecordandoPoo/Program.cs
@@ -11,6 +11,32 @@ Crear el método toString() para mostrar la información relativa al libro con e
 En el fichero main, crear 2 objetos Libro (los valores que se quieran) y mostrarlos por pantalla.
 Por último, indicar cuál de los 2 tiene más páginas.*/
 
+using programa_1;
+
 Console.WriteLine("Programa 1");
 
+//objetos y le damos los atributos
 
+libro libro1 = new libro(123,"libro1","Rubio",526) ;
+libro libro2 = new libro(456, "libro2", "Juan", 852);
+libro libro3 = new libro(789, "libro3", "Mejia", 159);
+
+
+//accedemos al metodo que hemos creado para escribir el libro
+
+Console.WriteLine(libro1.ToString());
+
+//comparamos que libro tiene mas paginas
+
+if (libro1.GetNumPaginas() > libro2.GetNumPaginas()) { Console.WriteLine("El libro 1 tiene mas paginas"); }
+else { Console.WriteLine("El libro 2 tiene mas paginas"); }
+
+//probando set
+
+libro1.SetNumPaginas(1024);
+
+Console.WriteLine("El libro 1 tiene "+libro1.GetNumPaginas()+" Paginas" );
+
+//volvemos a comparar
+if (libro1.GetNumPaginas() > libro2.GetNumPaginas()) { Console.WriteLine("El libro 1 tiene mas paginas"); }
+else { Console.WriteLine("El libro 2 tiene mas paginas"); }
--- RecordandoPoo/libro.cs
+++ RecordandoPoo/libro.cs
@@ -8,18 +8,67 @@ namespace programa_1
 {
     internal class libro
     {
-
+        //atributos
         private int ISBN;
         private string titulo;
         private string autor;
         private int numPaginas;
     
+
+        //constructores
         public libro(int isbn, string titulo, string autor, int numPaginas)
         {
             this.ISBN = isbn;
             this.titulo = titulo;
             this.autor = autor;
             this.numPaginas = numPaginas;
+        }
+
+        //metodos
+        public int GetISBN()
+        {
+            return ISBN;
+        }
+
+        public void SetISBN(int isbn)
+        {
+            ISBN = isbn;
+        }
+
+        public string GetTitulo()
+        {
+            return titulo;
+        }
+
+        public void SetTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+
+        public string GetAutor()
+        {
+            return autor;
+        }
+
+        public void SetAutor(string autor)
+        {
+            this.autor = autor;
+        }
+
+        public int GetNumPaginas()
+        {
+            return numPaginas;
+        }
+
+        public void SetNumPaginas(int numPaginas)
+        {
+            this.numPaginas = numPaginas;
+        }
+
+
+        public override string ToString()
+        {
+            return $"El libro {titulo} con ISBN {ISBN} creado por el autor {autor} tiene {numPaginas} páginas.";
         }
 
     }
